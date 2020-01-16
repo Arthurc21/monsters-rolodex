349f095b78144bd0dff23e4471a3689e6813e86c
Arthurc21 <artur.c2119@gmail.com>
2020-01-16 10:20:03 -0600

    agregando gh-pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import {CardList} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+    constructor() {
+        super(); //llama al constructor de React.component
+
+        this.state = {
+            monsters: [], //se va a actualizar la propiedad monster del objeto state
+            searchField: ''
+        };
+
+        //this.handleChange = this.handleChange.bind(this);
+        //unnecessary binding code
+    }
+
+    componentDidMount() {
+        //componentDidMount es un life-cycle method
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({monsters: users}));
+    }
+
+    handleChange = event => {
+        this.setState({searchField: event.target.value});
+    };
+
+    render() {
+        //para filtrar search sin modificar monster[] se usa "destructure"
+        const {monsters, searchField} = this.state; //genero 2 varaiables "monsters" & "searchField" y a ambas les asigno lo contenido en state
+        /*
+        equivale a:
+        const monsters = this.state.monsters;
+        const searchField = this.state.searchField;
+        */
+
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+        //el metodo includes verifica que el argumento ingresado se encuentre dentor del string al cual se le aplico el metodo
+
+        return (
+            <div className='App'>
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeholder='search monsters'
+                    handleChange={this.handleChange}
+                />
+        <CardList monsters={filteredMonsters /* o this.state.monsters*/}/>
     </div>
-  );
+    )
+        ;
+    }
 }
 
+
 export default App;
+
+/*<input
+type='search'
+placeholder='search monsters'
+onChange={e => {
+//onChange es un synthetic event
+//Cuando ocurre un cambio en el DOM debido a un usuario, el bot de react avisa a react del cambio
+//eso es un eynthetic event
+
+this.setState({searchField: e.target.value},
+() => console.log(this.state));
+//e de evento
+//setState es una funcion asincrona, cuando se escriba en el recuadro de busqueda
+// setState estara un evento (o caracter) atrasado
+//console.log(this.state);
+}}
+/>*/
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); // Remplaza el elemento con id 'root' por lo contenido en App
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
